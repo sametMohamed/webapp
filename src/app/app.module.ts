14e1fb66703a52d3f1864974e720256a8b67bd66
samet.m <mohamadsamet@outlook.com>
2018-02-15 18:03:37 +0100

    Add caroussels commande / dropdown command carreer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule, routingmodule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
